5d58c6e481de10413c12886dba65b5edbf344bae
savio <saviofreitas04@gmail.com>
2026-01-04 17:05:29 -0300

    Iniciação da API principal, criação de DTO's e Service

---
+++ src/main/java/com/portal/api/client/CarPostStoreClient.java
@@ -0,0 +1,45 @@
+package com.portal.api.client;
+
+import com.portal.api.dto.CarPostDTO;
+import com.portal.api.dto.OwnerPostDTO;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Objects;
+
+@Component
+public class CarPostStoreClient {
+
+    private final String USER_STORE_SERVICE_URL = "http://localhost:8080/user";
+    private final String POSTS_STORE_SERVICE_URL = "http://localhost:8080/sales";
+
+    @Bean
+    public RestTemplate restTemplate() {
+        return new RestTemplate();
+    }
+
+    @Autowired
+    RestTemplate restTemplate;
+
+    public List<CarPostDTO> carForSaleClient(){
+        ResponseEntity<CarPostDTO[]> responseEntity = restTemplate.getForEntity(POSTS_STORE_SERVICE_URL+"/cars", CarPostDTO[].class);
+        return Arrays.asList(Objects.requireNonNull(responseEntity.getBody()));
+    }
+
+    public void ownerPostsClient(OwnerPostDTO newUser){
+        restTemplate.postForEntity(USER_STORE_SERVICE_URL, newUser, OwnerPostDTO.class);
+    }
+
+    public void changeCarForSaleClient(CarPostDTO carPostDTO, String id){
+        restTemplate.put(POSTS_STORE_SERVICE_URL+"/car/"+id, carPostDTO, CarPostDTO.class);
+    }
+
+    public void deleteCarForSaleClient(String id){
+        restTemplate.delete(POSTS_STORE_SERVICE_URL+"/car/"+id);
+    }
+}
