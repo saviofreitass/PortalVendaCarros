8b45cbfadb55975bb5b2311bcabf0fd2f56c8921
savio <saviofreitas04@gmail.com>
2026-01-04 17:30:08 -0300

    Service Owner Post

---
+++ src/main/java/com/portal/api/service/owner/OwnerPostServiceImpl.java
@@ -0,0 +1,18 @@
+package com.portal.api.service.owner;
+
+import com.portal.api.client.CarPostStoreClient;
+import com.portal.api.dto.OwnerPostDTO;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class OwnerPostServiceImpl implements OwnerPostService {
+
+    private final CarPostStoreClient carPostStoreClient;
+
+    @Override
+    public void createOwnerCar(OwnerPostDTO ownerPostDTO) {
+        carPostStoreClient.ownerPostsClient(ownerPostDTO);
+    }
+}
